593486df4426dab6b42b3cd630d2ba84dec98167
Marcel Solle <msap3rtur3@gmail.com>
2019-06-30 22:02:03 +0200

    Added typings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': ['warn', 4],
-    'semi': ['warn', 'always'],
-    'space-before-function-paren': 'off',
-    'comma-dangle': 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': ['warn', 4],
+        'semi': ['warn', 'always'],
+        'space-before-function-paren': 'off',
+        'comma-dangle': 'off',
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
 };
